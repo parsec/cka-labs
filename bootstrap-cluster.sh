52004f5042a1eb23361a9694ba6ca1624e0382e8
parsec <beckmanseth@gmail.com>
2021-11-18 12:20:09 -0800

    Fixed some whitespace issues, removed some stuff

--- bootstrap-cluster.sh
+++ bootstrap-cluster.sh
@@ -1,12 +1,8 @@
-!#/bin/sh
-
-# bootstrap the cluster with args and calico manifest
+#!/bin/bash
 
 k3d cluster create "artemis" \
---servers 1\
---agents 2\
 --k3s-arg '--flannel-backend=none@server:*' \
---k3s-arg '--no-deploy traefik' \
---k3s-arg '--write-kubeconfig-mode 644' \
---k3s-arg '--kube-apiserver-arg=feature-gates=MixedProtocolLBService=true'\
---volume "./bootstrap/calico.yamls:/var/lib/rancher/k3s/server/manifests/calico.yaml"
+--k3s-arg '--no-deploy traefik@server:*' \
+--k3s-arg '--write-kubeconfig-mode 644@server:*' \
+--k3s-arg '--kube-apiserver-arg=feature-gates=MixedProtocolLBService=true@server:*' \
+--volume "/home/parsec/dev/cka-labs/bootstrap/calico.yamls:/var/lib/rancher/k3s/server/manifests/calico.yaml@server:*"
