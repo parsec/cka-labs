6874febda2fda264f35f8fd32144a9629b3a3f5c
parsec <beckmanseth@gmail.com>
2021-11-18 12:50:59 -0800

    Fixed some things with the bootstrap script

--- bootstrap-cluster.sh
+++ bootstrap-cluster.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 k3d cluster create "artemis" \
+--servers 1 \
+--agents 2 \
 --k3s-arg '--flannel-backend=none@server:*' \
---k3s-arg '--no-deploy traefik@server:*' \
---k3s-arg '--write-kubeconfig-mode 644@server:*' \
+--k3s-arg '--no-deploy=traefik@server:*' \
+--k3s-arg '--write-kubeconfig-mode=644@server:*' \
 --k3s-arg '--kube-apiserver-arg=feature-gates=MixedProtocolLBService=true@server:*' \
---volume "/home/parsec/dev/cka-labs/bootstrap/calico.yamls:/var/lib/rancher/k3s/server/manifests/calico.yaml@server:*"
+--volume "/home/parsec/dev/cka-labs/bootstrap/calico.yaml:/var/lib/rancher/k3s/server/manifests/calico.yaml@server:*"
